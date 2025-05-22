e134797cb8121025976fd5bfc62b584402d36729
Riad <riadk910@gmail.com>
2025-05-23 00:08:08 +0600

    Callback added with type

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -47,6 +47,29 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
 
+  callbacks: {
+    async session({ session, token }) {
+      if (session) {
+        session.user._id = token._id?.toString();
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.username = token.username;
+      }
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+      }
+
+      return token;
+    },
+  },
+
   pages: {
     signIn: "/sign-in",
   },

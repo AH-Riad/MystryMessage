6c14e5f8a278df7248de453115711b73599282c7
Riad <riadk910@gmail.com>
2025-06-03 18:01:49 +0600

    Route to check unique username

---
+++ src/app/api/unique-username-check/route.ts
@@ -0,0 +1,70 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/user";
+import { usernameValidation } from "@/schemas/signUpSchema";
+import { url } from "inspector";
+import { z } from "zod";
+
+const UsernameQuerySchema = z.object({
+  username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+  await dbConnect();
+  try {
+    const { searchParams } = new URL(request.url);
+    const queryParam = {
+      username: searchParams.get("username"),
+    };
+    //validate with zod
+    const result = UsernameQuerySchema.safeParse(queryParam);
+    if (!result.success) {
+      return Response.json(
+        {
+          success: false,
+          message: "Inavalid query parameters",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+    const { username } = result.data;
+
+    const existingVerifiedUser = await UserModel.findOne({
+      username,
+      isverified: true,
+    });
+    if (existingVerifiedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken",
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Username is unique",
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    console.log("Error checking username", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error checking username",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}

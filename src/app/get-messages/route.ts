aeeda7c1bf539e928b955d4229ad4798b628541d
Riad <riadk910@gmail.com>
2025-06-04 22:41:53 +0600

    Aggregation pipeline added

--- src/app/get-messages/route.ts
+++ src/app/get-messages/route.ts
@@ -3,6 +3,7 @@ import UserModel from "@/model/user";
 import { getServerSession } from "next-auth";
 import { User } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/options";
+import mongoose from "mongoose";
 
 export async function POST(request: Request) {
   await dbConnect();
@@ -19,5 +20,14 @@ export async function POST(request: Request) {
     );
   }
 
-  const userId = user._id;
+  const userId = new mongoose.Types.ObjectId(user._id);
+
+  try {
+    const user = await UserModel.aggregate([
+      { $match: { id: userId } },
+      { $unwind: "messages" },
+      { $sort: { "messages.createdAt": -1 } },
+      { $group: { _id: "$_id", messages: { $push: "messages" } } },
+    ]);
+  } catch (error) {}
 }

43d9d35593c329ebae2e671583de601a4daa8286
Riad <riadk910@gmail.com>
2025-06-14 20:55:29 +0600

    SignUp schema and regex updated

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -2,16 +2,16 @@ import { z } from "zod";
 
 export const usernameValidation = z
   .string()
-  .min(2, "Username must be atleast 2 characters")
-  .max(20, "Username must be no more than 20 characters")
+  .min(3, "Username must be at least 3 characters")
+  .max(40, "Username must be no more than 40 characters")
   .regex(
-    /^[a-zA-Z][a-zA-Z0-9_]{2,15}$/,
-    "Username must not contain special character"
+    /^[a-zA-Z][a-zA-Z0-9_]{2,39}$/,
+    "Username must not contain special characters"
   );
 
 export const signUpSchema = z.object({
   username: usernameValidation,
-  email: z.string().email({ message: "Inavalid email address" }),
+  email: z.string().email({ message: "Invalid email address" }),
   password: z
     .string()
     .min(6, { message: "password must be at least 6 characters" }),

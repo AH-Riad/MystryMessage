cd019f2ab843f600f1091dc07cbf5bcb262550b3
Riad <riadk910@gmail.com>
2025-06-15 13:18:36 +0600

    Try part of verifying code handled

---
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -0,0 +1,28 @@
+import { verifySchema } from "@/schemas/verifySchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+const VerifyAccount = () => {
+  const router = useRouter();
+  const params = useParams<{ username: string }>();
+
+  const form = useForm<z.infer<typeof verifySchema>>({
+    resolver: zodResolver(verifySchema),
+  });
+  const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+    try {
+      const response = await axios.post(`/api/verify-code`, {
+        username: params.username,
+        code: data.code,
+      });
+      router.replace("/api/sign-in");
+    } catch (error) {}
+  };
+  return <div>VerifyAccount</div>;
+};
+
+export default VerifyAccount;

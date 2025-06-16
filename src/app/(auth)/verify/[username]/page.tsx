c13a420aa3cf41eff3d2f8041c9d1265c5dc587b
Riad <riadk910@gmail.com>
2025-06-16 10:33:16 +0600

    Verify page added

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { verifySchema } from "@/schemas/verifySchema";
+import { ApiResponse } from "@/types/ApiResponse";
 import { zodResolver } from "@hookform/resolvers/zod";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useParams, useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
@@ -12,6 +25,9 @@ const VerifyAccount = () => {
 
   const form = useForm<z.infer<typeof verifySchema>>({
     resolver: zodResolver(verifySchema),
+    defaultValues: {
+      code: "",
+    },
   });
   const onSubmit = async (data: z.infer<typeof verifySchema>) => {
     try {
@@ -19,10 +35,49 @@ const VerifyAccount = () => {
         username: params.username,
         code: data.code,
       });
-      router.replace("/api/sign-in");
-    } catch (error) {}
+      toast.success("Success!", {
+        description: response.data.message,
+      });
+      router.replace("/sign-in");
+    } catch (error) {
+      console.log("Error in verifying user", error);
+      const AxiosError = error as AxiosError<ApiResponse>;
+      let errorMessage = AxiosError.response?.data.message;
+      toast.error("Signup failed", {
+        description: errorMessage,
+      });
+    }
   };
-  return <div>VerifyAccount</div>;
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Verify Your Account
+          </h1>
+          <p className="mb-4">Enter the verification code sent to your email</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="code"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Verify Code</FormLabel>
+                  <FormControl>
+                    <Input placeholder="code" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Verify</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default VerifyAccount;

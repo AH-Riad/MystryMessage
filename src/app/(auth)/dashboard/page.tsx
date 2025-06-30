37c4b7161ce965efad31c8de2586be813d53e718
Riad <riadk910@gmail.com>
2025-06-30 11:49:36 +0600

    Firing up dashboard page

--- src/app/(auth)/dashboard/page.tsx
+++ src/app/(auth)/dashboard/page.tsx
@@ -1,5 +1,28 @@
-import React from "react";
+"use client";
+import { Message } from "@/model/user";
+import { acceptMessageSchema } from "@/schemas/acceptMessageSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
-export default function DashboardPage() {
-  return <div>Dashboard content here</div>;
-}
+const dashboard = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [swtiching, isSwitching] = useState(false);
+
+  const handleMessage = (messgaeId: string) => {
+    setMessages(messages.filter((message) => message._id !== messgaeId));
+  };
+
+  const { data: session } = useSession();
+
+  const form = useForm({
+    resolver: zodResolver(acceptMessageSchema),
+  });
+
+  return <div>dashboard content will be available here</div>;
+};
+
+export default dashboard;

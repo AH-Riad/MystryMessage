6e6ee940cb35ac5d91a75c5b702f63dc77ebf200
Riad <riadk910@gmail.com>
2025-07-01 12:11:39 +0600

    Try part handled

--- src/app/(auth)/dashboard/page.tsx
+++ src/app/(auth)/dashboard/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { Message } from "@/model/user";
 import { acceptMessageSchema } from "@/schemas/acceptMessageSchema";
+import { ApiResponse } from "@/types/ApiResponse";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 
-const dashboard = () => {
+const Dashboard = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [swtiching, isSwitching] = useState(false);
+  const [swtiching, setisSwitching] = useState(false);
 
   const handleMessage = (messgaeId: string) => {
     setMessages(messages.filter((message) => message._id !== messgaeId));
@@ -22,7 +24,25 @@ const dashboard = () => {
     resolver: zodResolver(acceptMessageSchema),
   });
 
-  return <div>dashboard content will be available here</div>;
+  const { register, watch, setValue } = form;
+  const accceptMessages = watch("accepMessage");
+
+  const fetchAcceptMessage = useCallback(async () => {
+    setisSwitching(true);
+    try {
+      const response = await axios.get<ApiResponse>("/api/accept-message");
+      setMessages(response.data.Messages || []);
+      if (refresh) {
+        toast.error("Error!", {
+          description:
+            AxiosError.response?.data.message ||
+            "Failed to fetch message setting",
+        });
+      }
+    } catch (error) {}
+  }, [setValue]);
+
+  return <div>Dashboard content will be available here</div>;
 };
 
-export default dashboard;
+export default Dashboard;
